93597d44d57d2531e68c831400309fbdbe655996
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-04-27 03:19:54 +0200

    connection update for new node version compatibility

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+  process.env.DATABASE_URL || 'mongodb://127.0.0.1/argentBankDB' // 127.0.0.1
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
+    await mongoose.connect(databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true }) //, useUnifiedTopology: true
     console.log('Database successfully connected')
   } catch (error) {
     console.error(`Database Connectivity Error: ${error}`)
