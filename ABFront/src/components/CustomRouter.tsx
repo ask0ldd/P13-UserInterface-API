af4460b2964224724598531b3540cbac33a2113f
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-05-06 19:36:31 +0200

    user page base

--- ABFront/src/components/CustomRouter.tsx
+++ ABFront/src/components/CustomRouter.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import Login from '../pages/Login'
+import User from '../pages/User'
 
 function CustomRouter() {
     /*<Router basename="/P13-UserInterface-API/ABFront/"> needs to add basename into vite config*/
@@ -9,6 +10,7 @@ function CustomRouter() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/user" element={<User />} />
             </Routes>
         </Router>
     )
---
+++ ABFront/src/pages/User.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+
+function User(){
+    return(
+        <div className='App'>
+        <Header/>
+        <main className='main-user'>
+        </main>
+        <Footer/>
+      </div>        
+    )
+}
+
+export default User
