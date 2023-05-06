7594f3e1921b7ad7dfd054e4ea900e0b0fd04974
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-05-07 00:07:53 +0200

    user page : statement needs props

---
+++ ABFront/src/components/AccountStatement.tsx
@@ -0,0 +1,13 @@
+import '../style/AccountStatement.css'
+
+function AccountStatement(){
+ return(
+    <article className='statement-container'>
+      <div className='statement-datas'>
+
+      </div>
+    </article>
+ )
+}
+
+export default AccountStatement
--- ABFront/src/pages/User.tsx
+++ ABFront/src/pages/User.tsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import '../style/User.css'
+import AccountStatement from "../components/AccountStatement"
 
 function User(){
     return(
@@ -9,6 +10,10 @@ function User(){
         <main className='main-user'>
             <h1 className="h1-user">Welcome back<br/>Tony Jarvis!</h1>
             <button className="edit-button">Edit Name</button>
+            <h2 className="sr-only">Accounts</h2>
+            <AccountStatement/>
+            <AccountStatement/>
+            <AccountStatement/>
         </main>
         <Footer/>
       </div>        
