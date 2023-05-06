1fdec68547c3e53a80b7f5681dff62e44e59ac34
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-05-06 21:07:53 +0200

    user page last compo needed

--- ABFront/src/pages/User.tsx
+++ ABFront/src/pages/User.tsx
@@ -1,11 +1,14 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import '../style/User.css'
 
 function User(){
     return(
         <div className='App'>
         <Header/>
         <main className='main-user'>
+            <h1 className="h1-user">Welcome back<br/>Tony Jarvis!</h1>
+            <button className="edit-button">Edit Name</button>
         </main>
         <Footer/>
       </div>        
