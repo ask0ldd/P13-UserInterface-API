4690f7f322027afb802379fa94ba5b4eef9fcd9d
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-05-07 14:04:01 +0200

    login api call implem

---
+++ ABFront/src/utils/API.tsx
@@ -0,0 +1,54 @@
+/*function fetch(){
+    return
+}*/
+
+interface logs {
+    email : string
+    password : string
+}
+
+const api = "localhost:3001/api/v1/"
+
+export class API{
+    static async login({email, password} : logs){
+        try{
+            const response = await fetch(`${api}user/login`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({email, password})      
+            })
+
+            if(response.ok)
+            {
+                const userDatas = await response.json()
+                document.cookie = `id=${userDatas.userId}; Secure`
+                document.cookie = `token=${userDatas.token}; Secure`
+                window.location.href = "index.html" // change destination
+            }
+            else
+            {
+                switch(response.status)
+                { // change error code : cf swagger
+                    case 404:
+                        console.log(response.statusText)
+                        return {error : "User not found."}
+                    break;
+                    case 401:
+                        console.log(response.statusText)
+                        return {error : response.statusText}
+                    break;
+                    default:
+                        console.log(response.statusText)
+                        return {error : response.statusText}
+                }
+            }
+        }
+        catch
+        {
+            return {error : "Service Unavailable. Retry Later."}
+        }
+    }
+}
