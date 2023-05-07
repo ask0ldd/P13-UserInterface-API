73bbe98aed8ac439a45d388b14c2973264e680d7
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-05-07 12:22:51 +0200

    user page : props & buttons

--- ABFront/src/components/AccountStatement.tsx
+++ ABFront/src/components/AccountStatement.tsx
@@ -1,11 +1,21 @@
 import '../style/AccountStatement.css'
 
-function AccountStatement(){
+interface props {
+  accountType : string
+  accountId : string
+  balance : number
+  balanceStatus : string
+}
+
+function AccountStatement({accountType, accountId, balance, balanceStatus} : props){
  return(
     <article className='statement-container'>
       <div className='statement-datas'>
-
+        <h3 className='account-title'>Argent Bank {accountType} ({accountId})</h3>
+        <p className='account-balance'>${balance}</p>
+        <p className='account-status'>{balanceStatus}</p>
       </div>
+      <button>View Transactions</button>
     </article>
  )
 }
