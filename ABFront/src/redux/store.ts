b080c42c094ffe40006f787c0899dac4683c3e36
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-05-08 14:17:09 +0200

    configurestore redux

--- ABFront/src/redux/store.ts
+++ ABFront/src/redux/store.ts
@@ -1,5 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit"
+//import { PayloadAction } from "@reduxjs/toolkit"
+
+const logsReducer = () => {
+    return
+}
+
+const userInfosReducer = () => {
+    return
+}
 
 export const store = configureStore({
-    reducer: {}
-})
+    reducer: {
+      logs: logsReducer,
+      userInfos: userInfosReducer,
+    },
+  })
+
+// export those types so they can be used in globally
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export default store
