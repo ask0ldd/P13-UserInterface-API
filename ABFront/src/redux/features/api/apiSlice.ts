7397f37f50b451d417e1d4dac5fed1710cb5a889
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-05-09 17:09:21 +0200

    createpi rtk

--- ABFront/src/redux/features/api/apiSlice.ts
+++ ABFront/src/redux/features/api/apiSlice.ts
@@ -1,13 +1,27 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials, logout } from '../auth/authSlice'
+import { RootState } from '../../store'
 
-const baseQuery = fetchBaseQuery({
+/*const baseQuery = fetchBaseQuery({
     baseUrl : 'http://127.0.0.1:3001/api/v1/',
     credentials : 'include', // cookie sent with every query
     prepareHeaders : (headers, { getState }) => {
-        const token = getState().auth.token
+        const token = (getState() as RootState).auth.token
         if (token) {
             headers.set('authorization', `Bearer ${token}`)
         }
+        return headers
     },
+})*/
+
+export const apiSlice = createApi({
+    reducerPath : 'api',
+    baseQuery : fetchBaseQuery({ baseUrl : 'http://127.0.0.1:3001/api/v1/user/' }),
+    endpoints : (builder) => ({
+        login : builder.query({
+            query : () => '/login',
+        })
+    })
 })
+
+export const { useLoginQuery } = apiSlice
