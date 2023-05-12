ac8bb3860c171a2c0dfe54564a01c1230e7a7b7c
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-05-12 22:13:05 +0200

    header logout

--- ABFront/src/components/Header.tsx
+++ ABFront/src/components/Header.tsx
@@ -1,8 +1,21 @@
 import '../style/Header.css'
 import logo from '../assets/argentbank-logo.png'
 import { NavLink } from 'react-router-dom'
+import { logout } from '../redux/features/auth/authSlice'
+import { useTypedDispatch } from '../hooks/redux'
 
 function Header() {
+
+    const dispatch = useTypedDispatch()
+
+    function disco(){
+        document.cookie = "token=; Max-Age=0;"
+        document.cookie = "id=; Max-Age=0;"
+        window.location.href = "index.html"
+        dispatch(logout())
+    }
+    
+
     return (
         <header>
             <figure><img src={logo} alt="argent bank logo"/><h1 className="sr-only">Argent Bank</h1></figure>
