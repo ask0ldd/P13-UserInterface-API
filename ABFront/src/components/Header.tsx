5b80e62fabdc8b121cdcc9c397ef6565435768a4
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-05-12 23:38:20 +0200

    header logout ok

--- ABFront/src/components/Header.tsx
+++ ABFront/src/components/Header.tsx
@@ -2,7 +2,7 @@ import '../style/Header.css'
 import logo from '../assets/argentbank-logo.png'
 import { NavLink } from 'react-router-dom'
 import { logout } from '../redux/features/auth/authSlice'
-import { useTypedDispatch } from '../hooks/redux'
+import { useTypedDispatch, useTypedSelector } from '../hooks/redux'
 
 function Header() {
 
@@ -11,18 +11,18 @@ function Header() {
     function disco(){
         document.cookie = "token=; Max-Age=0;"
         document.cookie = "id=; Max-Age=0;"
-        window.location.href = "index.html"
         dispatch(logout())
     }
-    
+
+    const logged : boolean = useTypedSelector((state) => state.auth.logged)    
 
     return (
         <header>
             <figure><img src={logo} alt="argent bank logo"/><h1 className="sr-only">Argent Bank</h1></figure>
             <nav>
                 <NavLink className="signIn" to="/Login">
-                    <i className="fa fa-user-circle"/>
-                    Sign In
+                {!logged && <><i className="fa fa-user-circle"/>Sign In</>}
+                {logged && <><i className="fa fa-user-circle"/><span onClick={disco}>Logout</span></>}
                 </NavLink>
             </nav>
         </header>
