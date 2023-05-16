1d68aac5369e121ee65f8b50edff25658aa4136b
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-05-16 11:51:52 +0200

    accounts slice

---
+++ ABFront/src/redux/features/accounts/accountsSlice.ts
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars*/
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { createSlice } from "@reduxjs/toolkit";
+
+
+interface accountState{
+    accountType: string
+    accountId: string 
+    balance: number 
+    balanceStatus: string
+}
+
+const initialState : Array<accountState> = []
+
+export const accountsSlice = createSlice({
+    name : 'accounts', // so slice state will be reached through store.auth
+    initialState,
+    reducers : {
+        setAccountState : (state, action) => {
+            let newState = [...state]
+            newState = initialState
+            return newState[0] = action.payload
+        },
+        pushAccountState : (state, action) => {
+            const newState = [...state]
+            newState.push(action.payload)
+            return newState
+        },
+    }
+})
+
+export default accountsSlice.reducer
--- ABFront/src/redux/store.ts
+++ ABFront/src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit"
 //import { PayloadAction } from "@reduxjs/toolkit"
 import authReducer from './features/auth/authSlice'
+import accountsReducer from './features/accounts/accountsSlice'
 
 export const store = configureStore({
     reducer: {
       auth : authReducer,
+      accounts : accountsReducer,
     },
     devTools : true,
   })
