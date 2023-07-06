9d2d69b50dfe6869ed5689c99734ec5914850b31
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-07-06 03:13:22 +0200

    form validation error dealt with state

--- ABFront/src/pages/EditName.tsx
+++ ABFront/src/pages/EditName.tsx
@@ -5,11 +5,13 @@ import Header from "../components/Header"
 import Footer from "../components/Footer"
 import '../style/EditName.css'
 import AccountStatement from "../components/AccountStatement"
-import { useTypedSelector, useTypedDispatch } from "../hooks/redux"
 import { useNavigate } from "react-router-dom"
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef } from 'react'
+import { useTypedSelector, useTypedDispatch } from "../hooks/redux"
 import { updateNames } from "../redux/features/auth/authSlice"
+import { setEditNamesError } from "../redux/features/forms/formsSlice"
 import Validator from "../services/validators"
+
 
 
 function EditName(){
@@ -24,34 +26,31 @@ function EditName(){
     const firstname : string | null = useTypedSelector((state) => state.auth.firstname)
     const lastname : string | null = useTypedSelector((state) => state.auth.lastname)
     const token : string | null = useTypedSelector((state) => state.auth.token)
+    const editNamesValidationError : boolean = useTypedSelector((state) => state.forms.editNamesError)
 
     useEffect(() => {
         if (logged === false) navigate("/login")
     }, [logged])
-
-    const [validationError, setValidationError] = useState<boolean>(false)
 
     async function submit(e : React.FormEvent<HTMLFormElement>){
         e.preventDefault()
         e.stopPropagation()
 
         if(firstnameRef.current?.value == null || lastnameRef.current?.value == null) {
-            setValidationError(true)
+            dispatch(setEditNamesError({errorBoolean : true}))
             return false
         }
 
         const inputFirstname = (firstnameRef.current.value).trim()
         const inputLastname = (lastnameRef.current.value).trim()
 
-        // !!! needs to display an error
         if(!Validator.testName(inputFirstname) || !Validator.testName(inputLastname) || token == null) {
-            setValidationError(true)
+            dispatch(setEditNamesError({errorBoolean : true}))
             return false
         }
 
-        setValidationError(false)
+        dispatch(setEditNamesError({errorBoolean : false}))
         await dispatch(updateNames({firstName : inputFirstname, lastName : inputLastname}))
-        // deal with errors
         navigate("/user")
     }
 
@@ -69,7 +68,7 @@ function EditName(){
                     <input ref={firstnameRef} type="text" defaultValue={firstname != null ? firstname : undefined}/>
                     <input ref={lastnameRef} type="text" defaultValue={lastname != null ? lastname : undefined}/>
                 </div>
-                {validationError && <div style={{color:'red', height:'20px', fontSize:'14px', display:"flex", justifyContent:"center", alignItems:"center"}}>Invalid or empty field.</div>}
+                {editNamesValidationError && <div style={{color:'red', height:'20px', fontSize:'14px', display:"flex", justifyContent:"center", alignItems:"center"}}>Invalid or empty field.</div>}
                 <div className="button-grp">
                     <button type="submit" id="save-button" className="edit-button">Save</button>
                     <button onClick={cancel} id="cancel-button" className="edit-button">Cancel</button>
